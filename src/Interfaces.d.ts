8fad328429b1e721a61780d0c18f838b60d1395d
Kyle Stankovich <gamemakergml@gmail.com>
2020-02-05 11:11:30 -0500

    Removed custom .d.ts files as they were being ignored anyway.

--- src/Interfaces.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'stanks-utilities-node' {
-  /**
-   * Small interface for defining miscellaneous metadata with a value of type T.
-   */
-  export interface IMetadata<T> {
-    key:   string;
-    value: T;
-  }
-}
